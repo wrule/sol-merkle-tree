e2f38593183f33711f79188737a87d7cb8dc2f83
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-31 16:04:07 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -1,8 +1,8 @@
 import { ethers } from 'hardhat';
+import { BytesLike } from 'ethers';
 import { deployContract, init, meta } from './utils';
 import { X } from '../typechain-types';
 import { MerkleTree } from 'merkletreejs';
-import { SHA3 } from 'crypto-js';
 
 function hashPair(hash1: string, hash2: string) {
   const num1 = ethers.getBigInt(hash1);
@@ -14,7 +14,7 @@ function hashPair(hash1: string, hash2: string) {
 
 class OpenZeppelinMerleTree {
   public constructor(data: string[]) {
-    this.tree = new MerkleTree(data, ethers.keccak256, {
+    this.tree = new MerkleTree(data, this.smartKeccak256, {
       sortPairs: true,
       hashLeaves: true,
     });
@@ -28,14 +28,19 @@ class OpenZeppelinMerleTree {
     return '0x' + buffer.toString('hex');
   }
 
+  private smartKeccak256(data: BytesLike) {
+    if (typeof data !== 'string' || data.startsWith('0x')) return ethers.keccak256(data);
+    return ethers.keccak256(ethers.toUtf8Bytes(data));
+  }
+
   public get Root() { return this._root; }
 
   public getProof(item: string) {
-    return this.tree.getProof(ethers.keccak256(item)).map((route) => this.bufferString(route.data));
+    return this.tree.getProof(this.smartKeccak256(item)).map((route) => this.bufferString(route.data));
   }
 
   public verify(item: string, proof: string[]) {
-    return this.tree.verify(proof, ethers.keccak256(item), this.Root);
+    return this.tree.verify(proof, this.smartKeccak256(item), this.Root);
   }
 }
 
@@ -46,11 +51,12 @@ async function main() {
     '0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2',
     '0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db',
     '0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB',
+    '即使是',
   ];
   const tree = new OpenZeppelinMerleTree(data);
   console.log(tree.Root);
-  const proof = tree.getProof('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4');
-  console.log(tree.verify('0x5B38Da6a701c568545dCfcB03FcB875f56beddC4', proof));
+  const proof = tree.getProof('即使是');
+  console.log(tree.verify('即使是', proof));
 }
 
 async function dev() {
