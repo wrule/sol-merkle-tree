083f1aceef337fcf5d2a15b365085d0aec02e675
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-31 16:07:01 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -51,7 +51,6 @@ async function main() {
     '0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2',
     '0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db',
     '0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB',
-    '即使是',
   ];
   const tree = new OpenZeppelinMerleTree(data);
   console.log(tree.Root);
