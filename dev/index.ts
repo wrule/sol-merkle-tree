3c931412bf9f4a8855ea1c6ff933fb85a8dbbd39
jimao <jimao@jimaodeMacBook-Pro.local>
2023-10-31 13:58:38 +0800

    提交代码

--- dev/index.ts
+++ dev/index.ts
@@ -1,6 +1,8 @@
 import { ethers } from 'hardhat';
 import { deployContract, init, meta } from './utils';
 import { X } from '../typechain-types';
+import { MerkleTree } from 'merkletreejs';
+import { SHA3 } from 'crypto-js';
 
 function hashPair(hash1: string, hash2: string) {
   const num1 = ethers.getBigInt(hash1);
@@ -10,28 +12,22 @@ function hashPair(hash1: string, hash2: string) {
   return ethers.keccak256(pair);
 }
 
+function keccak256(message: string | CryptoJS.lib.WordArray) {
+  return SHA3(message, { outputLength: 256 });
+}
+
 async function main() {
   await meta();
-  console.log(hashPair(
-    '0x9d997719c0a5b5f6db9b8ac69a988be57cf324cb9fffd51dc2c37544bb520d65',
-    '0x4726e4102af77216b09ccd94f40daa10531c87c4d60bba7f3b3faf5ff9f19b3c',
-  ));
 
-  // const x = await deployContract<X>('X');
-  // const a = await x.verify([
-  //   '0x999bf57501565dbd2fdcea36efa2b9aef8340a8901e3459f4a4c926275d36cdb',
-  //   '0x4726e4102af77216b09ccd94f40daa10531c87c4d60bba7f3b3faf5ff9f19b3c',
-  // ],
-  //   '0xeeefd63003e0e702cb41cd0043015a6e26ddb38073cc6ffeb0ba3e808ba8c097',
-  //   '0x5931b4ed56ace4c46b68524cb5bcbf4195f1bbaacbe5228fbd090546c88dd229',
-  // );
-  // console.log(a);
-  // console.log(ethers.keccak256(`${
-  //   '0x4726e4102af77216b09ccd94f40daa10531c87c4d60bba7f3b3faf5ff9f19b3c'
-  // }${
-  //   '9d997719c0a5b5f6db9b8ac69a988be57cf324cb9fffd51dc2c37544bb520d65'
-  // }`));
-  // console.log(ethers.getBigInt('0x4726e4102af77216b09ccd94f40daa10531c87c4d60bba7f3b3faf5ff9f19b3c'));
+  const leaves = ['a', 'b', 'c'].map(x => keccak256(x));
+  const tree = new MerkleTree(leaves, keccak256);
+  const root = tree.getRoot().toString('hex')
+  const leaf = keccak256('a').toString();
+  const proof = tree.getProof(leaf);
+  console.log(tree.verify(proof, leaf, root));
+
+  // console.log(keccak256('a').toString());
+  // console.log(ethers.keccak256(ethers.toUtf8Bytes('a')));
 }
 
 async function dev() {
